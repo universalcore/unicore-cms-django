538a008d9d21b0d6e27ae8d473af5f337a38c2a2
Milton Madanda <madandat@gmail.com>
2014-09-13 11:45:02 +0200

    re-enable grappeli

--- project/settings.py
+++ project/settings.py
@@ -145,7 +145,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    #'grappelli',
+    'grappelli',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
