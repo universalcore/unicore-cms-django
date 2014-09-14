3e87381087f0d65e94d49e5b1e031119081f7e7a
Milton Madanda <madandat@gmail.com>
2014-09-14 16:45:05 +0200

    ensure admin redirects to /

--- project/urls.py
+++ project/urls.py
@@ -2,14 +2,19 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from adminplus.sites import AdminSitePlus
 
 admin.site = AdminSitePlus()
 admin.autodiscover()
 
+
+def redirect_to_root(request):
+    return HttpResponseRedirect('/')
+
 urlpatterns = patterns(
     '',
-    url(r'^', include(admin.site.urls)),
+    url(r'^admin/', redirect_to_root),
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^ckeditor/', include('ckeditor.urls')),
+    url(r'^', include(admin.site.urls)),
 )
