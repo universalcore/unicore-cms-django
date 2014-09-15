464364acdc157c6443c451acac5aa647cfcb7415
Milton Madanda <madandat@gmail.com>
2014-09-15 09:42:41 +0200

    use redirectview

--- project/urls.py
+++ project/urls.py
@@ -1,20 +1,15 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from django.http import HttpResponseRedirect
+from django.views.generic.base import RedirectView
+
 from adminplus.sites import AdminSitePlus
 
 admin.site = AdminSitePlus()
 admin.autodiscover()
 
-
-def redirect_to_root(request):
-    return HttpResponseRedirect('/')
-
 urlpatterns = patterns(
     '',
-    url(r'^admin/', redirect_to_root),
+    url(r'^admin/', RedirectView.as_view(url='/')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^', include(admin.site.urls)),
 )
