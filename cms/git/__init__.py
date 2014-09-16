be2e9c64cf4803dcc473433c3182bc1c1bb9cd92
Milton Madanda <madandat@gmail.com>
2014-09-16 10:37:01 +0200

    specify excpetion in try block

--- cms/git/__init__.py
+++ cms/git/__init__.py
@@ -6,7 +6,7 @@ from django.conf import settings
 def init_repository():
     try:
         repo = pygit2.Repository(settings.GIT_REPO_PATH)
-    except:
+    except KeyError:
         repo = pygit2.init_repository(settings.GIT_REPO_PATH, False)
     repo_helper.checkout_all_upstream(repo)
     return repo
