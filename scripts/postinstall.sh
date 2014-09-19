1e6ad74556dd151bbc994d86cfc2b67c53ff9002
Milton Madanda <madandat@gmail.com>
2014-09-19 12:58:21 +0200

    ensure all settigns modules run syncdb

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,8 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
+settings="${INSTALLDIR}/${REPO}/project/*_settings.py"
 
-$manage syncdb --noinput --no-initial-data --migrate
-$manage collectstatic --noinput
+for s in $settings
+do
+    DJANGO_SETTINGS_MODULE='project."$(basename $s .py)"' $manage syncdb --noinput --no-initial-data --migrate
+    DJANGO_SETTINGS_MODULE='project."$(basename $s .py)"' $manage collectstatic --noinput
+done
