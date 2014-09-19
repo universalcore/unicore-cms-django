db1fc8b4afe16d62cb1a89971eccd4930e7bde6b
Milton Madanda <madandat@gmail.com>
2014-09-19 13:03:28 +0200

    fix post install

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,6 +3,6 @@ settings="${INSTALLDIR}/${REPO}/project/*_settings.py"
 
 for s in $settings
 do
-    DJANGO_SETTINGS_MODULE='project."$(basename $s .py)"' $manage syncdb --noinput --no-initial-data --migrate
-    DJANGO_SETTINGS_MODULE='project."$(basename $s .py)"' $manage collectstatic --noinput
+    DJANGO_SETTINGS_MODULE="project.$(basename $s .py)" $manage syncdb --noinput --no-initial-data --migrate
+    DJANGO_SETTINGS_MODULE="project.$(basename $s .py)" $manage collectstatic --noinput
 done
