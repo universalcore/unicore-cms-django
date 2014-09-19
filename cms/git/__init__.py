2099a8939147f00b96fa59439d95ce289dbcf3d3
Milton Madanda <madandat@gmail.com>
2014-09-19 15:06:44 +0200

    use credentials when cloning repo

--- cms/git/__init__.py
+++ cms/git/__init__.py
@@ -4,9 +4,24 @@ from gitmodel.utils import repo_helper
 from django.conf import settings
 
 
+def get_credentials():
+    if hasattr(settings, 'SSH_PUBKEY_PATH') and hasattr(
+            settings, 'SSH_PRIVKEY_PATH'):
+        return pygit2.Keypair(
+            'git',
+            settings.SSH_PUBKEY_PATH,
+            settings.SSH_PRIVKEY_PATH,
+            settings.SSH_PASSPHRASE)
+    return None
+
+
 def init_repository():
     if settings.GIT_REPO_URL and not os.path.exists(settings.GIT_REPO_PATH):
-        pygit2.clone_repository(settings.GIT_REPO_URL, settings.GIT_REPO_PATH)
+        credentials = get_credentials()
+        pygit2.clone_repository(
+            settings.GIT_REPO_URL,
+            settings.GIT_REPO_PATH,
+            credentials=credentials)
 
     try:
         repo = pygit2.Repository(settings.GIT_REPO_PATH)
