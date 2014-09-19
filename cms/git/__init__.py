c5517d4c5f9a8e1d16c9fdfad47c51406677bdeb
Milton Madanda <madandat@gmail.com>
2014-09-19 14:26:33 +0200

    clone repo on init

--- cms/git/__init__.py
+++ cms/git/__init__.py
@@ -1,9 +1,13 @@
+import os
 import pygit2
 from gitmodel.utils import repo_helper
 from django.conf import settings
 
 
 def init_repository():
+    if settings.GIT_REPO_URL and not os.path.exists(settings.GIT_REPO_PATH):
+        pygit2.clone_repository(settings.GIT_REPO_URL, settings.GIT_REPO_PATH)
+
     try:
         repo = pygit2.Repository(settings.GIT_REPO_PATH)
     except KeyError:
