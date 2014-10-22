225d44e4d50cfa29750e26fca326134e7f3ca691
Simon de Haan <simon@praekeltfoundation.org>
2014-10-22 17:41:27 +0200

    removing unused code

--- cms/utils.py
+++ cms/utils.py
@@ -8,13 +8,3 @@ def push_to_git(repo_path, index_prefix):
         repo = workspace.repo
         remote = repo.remote()
         remote.push()
-
-
-def get_author_from_user(user):
-    author = None
-    if user:
-        author = (
-            user.username,
-            user.email if user.email else 'author@unicore.io'
-        )
-    return author
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 coverage erase
 coverage run `which py.test` --ds=test_settings --verbose cms $1
 coverage report
