c1e269bb6f26b9269ddbfaf92519145df9292659
Milton Madanda <madandat@gmail.com>
2015-02-17 14:12:45 +0200

    only quote variables in postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,7 +2,7 @@
 
 pip="${VENV}/bin/pip"
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
-settings=`find "${INSTALLDIR}/${REPO}/project/" -name "*_settings.py"`
+settings=`find "${INSTALLDIR}"/"${REPO}"/project/ -name "*_settings.py"`
 $pip install -r "${INSTALLDIR}/${REPO}/requirements.txt"
 
 cd "${INSTALLDIR}/${REPO}/"
